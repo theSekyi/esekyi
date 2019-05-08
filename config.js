9e945811fd846ee59bc7b2633515950d2c99c64a
theSekyi <emmanuelkwamesekyi@gmail.com>
2019-05-08 11:12:45 +0000

    Added description

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://www.esekyi.com/',
   title: 'Emmanuel Sekyi',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  subtitle: 'Machine learnist, Software Practitioner(web), Wine Evangelist',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-139872255-1',
   menu: [
     {
       label: 'Articles',
